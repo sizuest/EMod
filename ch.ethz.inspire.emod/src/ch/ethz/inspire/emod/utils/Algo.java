6d6f83eec1d4ab9e80aa0532e618c637da21c119
David Hampl <dhampl@ethz.ch>
2011-07-04 15:14:31 +0000

    added header comment in Algo.java

--- ch.ethz.inspire.emod/src/ch/ethz/inspire/emod/utils/Algo.java
+++ ch.ethz.inspire.emod/src/ch/ethz/inspire/emod/utils/Algo.java
@@ -1,6 +1,14 @@
-/************************************
- * 
- * 
+/***********************************
+ * $Id$
+ *
+ * $URL$
+ * $Author$
+ * $Date$
+ * $Rev$
+ *
+ * Copyright (c) 2011 by Inspire AG, ETHZ
+ * All rights reserved
+ *
  ***********************************/
 
 package ch.ethz.inspire.emod.utils;
