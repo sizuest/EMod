d53323b9e96cdd2380b954e2cca15c4ac51d77a4
Manuel Nick <manick@ethz.ch>
2014-06-04 17:32:52 +0000

    manick: chg: LinkingGUI.java (adapt to new Machine.getOutputList(), short for statements)

--- ch.ethz.inspire.emod/src/ch/ethz/inspire/emod/gui/LinkingGUI.java
+++ ch.ethz.inspire.emod/src/ch/ethz/inspire/emod/gui/LinkingGUI.java
@@ -46,8 +46,8 @@ public class LinkingGUI {
 	    }
 
 	public void openLinkingGUI(){
-        	shell = new Shell(Display.getCurrent());
-		
+			shell = new Shell(Display.getCurrent());
+			
 	        shell.setText(LocalizationHandler.getItem("app.gui.linking.title"));
 	    	shell.setLayout(new GridLayout(1, false));
 	    	
@@ -60,6 +60,11 @@ public class LinkingGUI {
 	    	
 			//get List of current Machinecomponents
 			ArrayList<MachineComponent> components = Machine.getInstance().getMachineComponentList();
+			
+			if (components.size()<2){
+				shell.dispose();
+				//TODO manick: end function!!!
+			}
 			
 	    	//set table for the linking
 			//SOURCE http://www.java2s.com/Code/Java/SWT-JFace-Eclipse/SWTTableSimpleDemo.htm Imported for function control
@@ -82,56 +87,31 @@ public class LinkingGUI {
 	    		    }
 	    		    
 	    		    //iterate twice, first over all existing components
-	    		    for (int j=0; j < components.size(); j++){
-	    		    	
+	    		    for (MachineComponent mc:components){
 	    		    	//for each existing component, get the list of inputs
-	    		    	List<IOContainer> inputs = components.get(j).getComponent().getInputs();
+	    		    	List<IOContainer> inputs = mc.getComponent().getInputs();
 	    		    	
 	    		    	//iterate a second time, over the inputs of the current component
-	    		    	for(int k=0; k < inputs.size(); k++){
-	    		    		
+	    		    	for(IOContainer io:inputs){
 		    		    	//create table item for each input of the current component
 		    		    	TableItem item = new TableItem(tableLinkingView, SWT.NULL);
 	    		    		
-		    		    	//set content of table item as following: name of component, name of input, unit of input, possible outputs
-		    		    	item.setText(0, components.get(j).getName());
-		    		    	item.setText(1, inputs.get(k).getName());
-		    		    	item.setText(2, inputs.get(k).getUnit().toString());
+		    		    	//set content of table item as following: name of component, name of input, unit of input, possible outputs		    		    	
+		    		    	item.setText(0, mc.getName());
+		    		    	item.setText(1, io.getName());
+		    		    	item.setText(2, io.getUnit().toString());
 
-		    		    	
-		    		    	
-		    		    	
 		    		    	//for column of the possible outputs, a drop down combo is needed
 		  		        	TableEditor editor = new TableEditor(tableLinkingView);
 		  		        	Combo comboOutputs = new Combo(tableLinkingView, SWT.DROP_DOWN);
-		    		    	
-		    		    	/*/
-		    		    	
-		    		    	//TODO manick: NullPointerException???
-		    		    	
-		    		    	
-		    		    	//get the outputs, that can be matched to the current input
-		    				
-		    		    	//ArrayList<IOContainer> outputs = Machine.getOutputList();
-		    		        //ArrayList<IOContainer> outputs = Machine.getOutputList(inputs.get(k).getUnit());
-		    		        ArrayList<IOContainer> outputs = Machine.getOutputList(components.get(j), inputs.get(k).getUnit());
-
-		  		        	//iterate over the possivle outputs and add them to the combo
- 		  		        	for(int l=0; l < outputs.size(); l++){
-		  		        		comboOutputs.setItem(l, outputs.get(l).getName());
-		  		        	}
-
- 		  		        	//*/
-	  		        	
-		  		        	ArrayList<String> outputs = Machine.getOutputList(components.get(j), inputs.get(k).getUnit());
-		  		        	String[] items = outputs.toArray(new String[outputs.size()]);
 		  		        	
+							ArrayList<String> outputs = Machine.getOutputList(mc, io.getUnit());	
+		  		        	String[] items = outputs.toArray(new String[outputs.size()]);	  		        	
 		  		        	//String items[] = {"Output 1", "Output 2", "Output 3", "and so on"};
 		  		        	comboOutputs.setItems(items);
  		  		        	
- 		  		        	
  		  		        	//combo Selection Listener
- 		  		        	//TODO manick: must contain -> set Linking??
+ 		  		        	//TODO manick: must contain -> write Linking to file? or in save button?
 		  		        	comboOutputs.addSelectionListener(new SelectionListener(){
 		  		        		public void widgetSelected(SelectionEvent event){
 				        		
@@ -153,44 +133,13 @@ public class LinkingGUI {
 	  
 	    		    }
 	    		    
-	    		    
-	    		    /*/
-	    		    for (int i=0; i < inputList.size(); i++){
-	    		    	TableItem item = new TableItem(tableLinkingView, SWT.MULTI);
-	    		    	item.setText(0, inputList.get(i).getName());
-
-	  		        	TableEditor editor = new TableEditor(tableLinkingView);
-	  		        	Combo comboOutputs = new Combo(tableLinkingView, SWT.DROP_DOWN);
-	  		        	String items[] = {"Output 1", "Output 2", "Output 3", "and so on"};
-	  		        	comboOutputs.setItems(items);
-	  		        	
-	  		        	comboOutputs.addSelectionListener(new SelectionListener(){
-	  		        		public void widgetSelected(SelectionEvent event){
-			        		
-	  		        			String string = (String) event.data;
-	  		        			System.out.println("Ouput selected: " + string);
-	  		        		}
-	  		        		public void widgetDefaultSelected(SelectionEvent event){
-			        		
-	  		        		}
-	  		        	});
-	  		        	
-	  		        	comboOutputs.pack();
-	  		        	editor.minimumWidth = comboOutputs.getSize().x;
-	  		        	editor.horizontalAlignment = SWT.LEFT;
-	  		        	editor.setEditor(comboOutputs, item, 1);
-	    		    }
-	    		    */
-	    		    
-	    		    
 			        //Tabelle schreiben
 			        TableColumn[] columns = tableLinkingView.getColumns();
 			        for (int i = 0; i < columns.length; i++) {
 			          columns[i].pack();
 			        }
 
-			        //SOURCE http://www.java2s.com/Code/Java/SWT-JFace-Eclipse/SWTTableSimpleDemo.htm Imported for function control   	
-
+			//SOURCE http://www.java2s.com/Code/Java/SWT-JFace-Eclipse/SWTTableSimpleDemo.htm Imported for function control   	
 	    	Button buttonSave = new Button(shell, SWT.NONE);
 	    	buttonSave.setText(LocalizationHandler.getItem("app.gui.save"));
 			gridData = new GridData(GridData.END, GridData.END, false, false);
