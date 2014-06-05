5c9d73535529c464d02344ad9e9a4d369a586d66
Simon Züst <sizuest@gmail.com>
2014-06-05 05:40:51 +0000

    add: DynamicState

---
+++ ch.ethz.inspire.emod/src/ch/ethz/inspire/emod/simulation/DynamicState.java
@@ -0,0 +1,184 @@
+/***********************************
+ * $Id$
+ *
+ * $URL$
+ * $Author$
+ * $Date$
+ * $Rev$
+ *
+ * Copyright (c) 2014 by Inspire AG, ETHZ
+ * All rights reserved
+ *
+ ***********************************/
+
+package ch.ethz.inspire.emod.simulation;
+
+import ch.ethz.inspire.emod.model.units.Unit;
+import ch.ethz.inspire.emod.utils.ConfigReader;
+import ch.ethz.inspire.emod.utils.Defines;
+import ch.ethz.inspire.emod.utils.PropertiesHandler;
+
+
+/**
+ * Implements an initial condition for a {@link APhysicalComponent} 
+ * @author sizuest
+ *
+ */
+public class DynamicState {
+	private String name;
+	private double value, lastValue;
+	private double initialValue;
+	private double timestep;
+	private Unit unit;
+	private String parent;
+	
+	/**
+	 * Initial Condition for {@link APhysicalComponent}
+	 * @param name	Name of the initial condition
+	 * @param unit  Unit
+	 */
+	public DynamicState(String name, Unit unit) {
+		this.name = name;
+		this.value = Double.NaN;
+		this.lastValue = Double.NaN;
+		this.initialValue = Double.NaN;
+		this.unit = unit;
+		this.parent = "";
+		this.timestep = 0;
+	}
+	
+	/**
+	 * Sets the value of the initial condition
+	 * @param value
+	 */
+	public void setValue(double value){
+		this.lastValue = this.value;
+		this.value     = value;
+	}
+	
+	/**
+	 * Sets the value of the initial condition
+	 * @param initialValue
+	 */
+	public void setInitialCondition(double initialValue){
+		this.initialValue = initialValue;
+		setInitialCondition();
+	}
+	public void setInitialCondition(){
+		this.value     = this.initialValue;
+		this.lastValue = this.initialValue;
+	}
+	
+	/**
+	 * Sets the parent of the initial condition
+	 * @param parent
+	 */
+	public void setParent(String parent){
+		this.parent = parent;
+	}
+	
+	/**
+	 * Sets the timestep duration [s]
+	 * @param timestep
+	 */
+	public void setTimestep(double timestep){
+		this.timestep = timestep;
+	}
+	
+	/**
+	 * @return current value
+	 */
+	public double getValue(){
+		return this.value;
+	}
+	
+	/**
+	 * @return initial condition value
+	 */
+	public double getInitialValue(){
+		return initialValue;
+	}
+	
+	/**
+	 * @return numerical derivate of value
+	 */
+	public double getTimeDerivate(){
+		return (value-lastValue)/timestep;
+	}
+	
+	/**
+	 * @return initial condition name
+	 */
+	public String getName(){
+		return this.name;
+	}
+	
+	/**
+	 * @return initial condition parent name
+	 */
+	public String getParent(){
+		return this.parent;
+	}
+	
+	/**
+	 * @return initial condition unit
+	 */
+	public Unit getUnit(){
+		return this.unit;
+	}
+	
+	/**
+	 * Generates a unique IC name containing the parent and the IC names
+	 * @return parent.name
+	 */
+	public String getInitialConditionName(){
+		return parent+"."+name+"initialValue";
+	}
+	
+	/**
+	 * Loads the initial condition from the simulation file
+	 * @throws Exception 
+	 */
+	public void loadInitialCondition() throws Exception{
+		
+		if(this.parent.equals("")) 
+			throw new Exception("Can't save initial condition: No parent set!");
+		
+		try {
+			ConfigReader initCond = new ConfigReader( configPath() );
+			value = initCond.getDoubleValue(getInitialConditionName());
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			value = Double.NaN;
+		}
+	}
+	
+	
+	/**
+	 * Saves the initial condition
+	 * @throws Exception 
+	 */
+	public void saveInitialCondition() throws Exception{
+		
+		if(this.parent.equals(""))
+			throw new Exception("Can't save initial condition: No parent set!");
+		
+		try {
+			ConfigReader initCond = new ConfigReader( configPath() );
+			initCond.setValue(getInitialConditionName(), this.value);
+		}
+		catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private String configPath(){
+		return PropertiesHandler.getProperty("app.MachineDataPathPrefix")+
+			      "/"+PropertiesHandler.getProperty("sim.MachineName")+"/"+Defines.SIMULATIONCONFIGDIR+"/"+
+			      PropertiesHandler.getProperty("sim.SimulationConfigName")+"/"+Defines.SIMULATIONCONFIGFILE;
+	}
+	
+}
